3638c06a55701ce43b2ba22b8982d79a5a35723d
I-mikan-I <67881102+I-mikan-I@users.noreply.github.com>
2023-04-30 16:03:15 +0200

    Add license headers

--- src/ir.rs
+++ src/ir.rs
@@ -1,3 +1,5 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
 //! Intermediate Representation
 //!
 //! The compiler's IR is a three-address, register based code with an infinite number of registers.
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,3 +1,5 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
 mod sheaf_table;
 mod union_find;
 pub use sheaf_table::*;
